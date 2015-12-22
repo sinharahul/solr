303215c7bf3e6331bb4ef424d20f54e15d7b3f39
sinharahul <sinharahul@gmail.com>
2015-12-21 20:40:35 -0500

    solr

--- solr/hello.rb
+++ solr/hello.rb
@@ -6,5 +6,11 @@ solr = RSolr.connect(:read_timeout => 120, :open_timeout => 120)
 # send a request to /select
 response = solr.get 'techproducts/select', :params => {:q => '*:*'}
 
+hd= solr.get 'techproducts/select', :params => {:q => 'cat:"hard drive"'}
+
+pop = solr.get 'techproducts/select', :params => {:q => 'inStock:true AND  popularity:[6 TO *]'}
+
 puts response
 
+puts hd
+puts pop
