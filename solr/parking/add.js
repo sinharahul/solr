f918ba7eba5c6a2c16d1544d90930e060858e8bd
sinharahul <sinharahul@gmail.com>
2015-12-31 20:31:57 -0500

    added node.js client

--- solr/parking/add.js
+++ solr/parking/add.js
@@ -3,10 +3,11 @@ var solr = require('solr-client');
 
 // Create a client
 var client = solr.createClient('localhost',8983,'parking');
-
+client.autoCommit = true;
+for(var i=0 ; i< 1000 ;i++) {
 // Add a new document
 client.add(
-{ "id" : "3" , "p_lot_name" : "DayCare" , "p_lot_id" : "Lot1" , "p_lot_customer_name" : "Sweety" , "p_lot_date_start" : "NOW" , "p_lot_date_end" : "NOW+1MONTHS" , "p_lot_amount" : "120" , "p_lot_amount_paid" : "true" }
+{ "id" : i , "p_lot_name" : "DayCare" , "p_lot_id" : "Lot1" , "p_lot_customer_name" : "Sweety" , "p_lot_date_start" : "NOW" , "p_lot_date_end" : "NOW+1MONTHS" , "p_lot_amount" : "120" , "p_lot_amount_paid" : "true" }
 ,function(err,obj){
    if(err){
       console.log(err);
@@ -14,3 +15,5 @@ client.add(
       console.log('Solr response:', obj);
    }
 });
+
+}
