2274c03d2628f19acb71c368636dc82a54e45e5c
sinharahul <sinharahul@gmail.com>
2016-01-09 00:56:33 -0500

    more bond examples

--- bond.rb
+++ bond.rb
@@ -3,15 +3,13 @@ class Bond
   def price
     puts "Calculating price of bond #{@name}"
     # coupanrate = 4% ,par 100, amt=4
-    puts "par=#{@par} and couponrate=#{@coupanrate}"
+    puts "par=#{@par} and couponrate=#{@coupanrate} and discount rate=#{@discountrate}"
     amt=@par*@coupanrate/100.0 
     denom=1.0+@discountrate/100
-    puts "Coupan = #{amt}"
-    puts "Denom=#{denom}"
     sum=0
     for i in 1..@timetomaturity-1
       sum+=amt/(denom ** i)
-      puts "i=#{i} sum=#{sum}"
+#      puts "i=#{i} sum=#{sum}"
     end
     sum+=(@par+amt)/(denom ** @timetomaturity)
     puts "Bond market price is #{sum.round(3)}"
@@ -27,3 +25,18 @@ ibm.timetomaturity=5
 
 puts ibm
 ibm.price
+
+apple=Bond.new
+apple.name="apple premium"
+apple.par=100.0
+apple.coupanrate=6.0
+apple.discountrate=6.0
+apple.timetomaturity=5
+apple.price
+
+premium=Bond.new
+premium.par=100
+premium.coupanrate=8.0
+premium.discountrate=6.0
+premium.timetomaturity=5
+premium.price
